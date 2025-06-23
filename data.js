7d1f48848fb8219e4f730697d225e0864c22bd3d
Nhật Minh <jayky2k9@gmail.com>
2025-06-23 13:15:16 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,55 @@
+const linkData = [
+    {
+        type: 'button',
+        id: 'subscribeBtn',
+        iconClass: 'fas fa-bell',
+        title: 'Subscribe',
+        description: '',
+        url: 'YOUR_SUBSCRIBE_LINK_HERE',
+        linkTypeClass: 'subscribe-button'
+    },
+    {
+        type: 'link',
+        iconClass: 'fab fa-apple',
+        title: 'Kho IPA',
+        description: 'Kho ipa siêu vip dành cho bạn',
+        url: 'https://ontop2k9.github.io/shareipa/',
+        linkTypeClass: 'apple-link'
+    },
+    {
+        type: 'link',
+        iconClass: 'fab fa-telegram-plane',
+        title: 'Grup Telegram',
+        description: 'Trả Đổi Công Nghệ',
+        url: 'YOUR_TELEGRAM_GROUP_LINK_HERE',
+        linkTypeClass: 'telegram-link'
+    },
+    {
+        type: 'link',
+        iconClass: 'fab fa-apple',
+        title: 'Website bán chứng chỉ',
+        description: '',
+        url: 'YOUR_CERTIFICATE_SALES_WEBSITE_LINK_HERE',
+        linkTypeClass: 'apple-link'
+    },
+    {
+        type: 'link',
+        iconImage: 'https://i.imgur.com/bXed84d.png', // Ví dụ: 'https://i.imgur.com/your-ontop-logo.png'
+        title: 'ON^TOP IOS',
+        description: 'Server discord',
+        url: 'YOUR_ONTOP_DISCORD_SERVER_LINK_HERE',
+        linkTypeClass: 'ontap-link'
+    },
+    {
+        type: 'sectionTitle',
+        title: 'App IPA'
+    },
+    {
+        type: 'link',
+        iconClass: 'fab fa-youtube',
+        title: 'Youtube mod ipa',
+        description: 'Unlock pip,chặn quảng cáo',
+        url: 'YOUR_YOUTUBE_MOD_IPA_LINK_HERE',
+        linkTypeClass: 'youtube-link'
+    }
+];
---
+++ script.js
@@ -0,0 +1,86 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.container');
+    const linksContainer = document.getElementById('linksContainer');
+
+    function createLinkElement(data) {
+        if (data.type === 'sectionTitle') {
+            const sectionTitle = document.createElement('div');
+            sectionTitle.classList.add('section-title');
+            sectionTitle.textContent = data.title;
+            return sectionTitle;
+        }
+
+        const element = data.type === 'button' ?
+                        document.createElement('button') :
+                        document.createElement('a');
+
+        element.classList.add('link-item');
+        if (data.linkTypeClass) {
+            element.classList.add(data.linkTypeClass);
+        }
+        if (data.id) {
+            element.id = data.id;
+        }
+
+        const iconDiv = document.createElement('div');
+        iconDiv.classList.add('link-icon');
+        if (data.iconImage) {
+            const img = document.createElement('img');
+            img.src = data.iconImage;
+            img.alt = data.title + ' Logo';
+            img.classList.add('custom-icon');
+            iconDiv.appendChild(img);
+        } else if (data.iconClass) {
+            const icon = document.createElement('i');
+            icon.classList.add(...data.iconClass.split(' '));
+            iconDiv.appendChild(icon);
+        }
+        element.appendChild(iconDiv);
+
+        const contentDiv = document.createElement('div');
+        contentDiv.classList.add('link-content');
+
+        const titleDiv = document.createElement('div');
+        titleDiv.classList.add('link-title');
+        titleDiv.textContent = data.title;
+        contentDiv.appendChild(titleDiv);
+
+        if (data.description) {
+            const descriptionDiv = document.createElement('div');
+            descriptionDiv.classList.add('link-description');
+            descriptionDiv.textContent = data.description;
+            contentDiv.appendChild(descriptionDiv);
+        }
+        element.appendChild(contentDiv);
+
+        if (data.url) {
+            element.addEventListener('click', (event) => {
+                event.preventDefault();
+                window.location.href = data.url;
+                // Hoặc để mở trong tab mới: window.open(data.url, '_blank');
+            });
+        }
+
+        return element;
+    }
+
+    function renderLinks() {
+        linksContainer.innerHTML = '';
+        linkData.forEach(item => {
+            const element = createLinkElement(item);
+            linksContainer.appendChild(element);
+        });
+    }
+
+    renderLinks();
+
+    const linkItems = document.querySelectorAll('.link-item');
+
+    container.addEventListener('animationend', () => {
+        linkItems.forEach((item, index) => {
+            setTimeout(() => {
+                item.classList.add('show');
+            }, index * 100);
+        });
+    }, { once: true });
+});
