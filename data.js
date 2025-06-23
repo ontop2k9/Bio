8411e4e682bacf48b8b3c6687e25cf1bde93568c
Nhật Minh <jayky2k9@gmail.com>
2025-06-23 13:18:35 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,5 @@
 const linkData = [
     {
-        type: 'button',
-        id: 'subscribeBtn',
-        iconClass: 'fas fa-bell',
-        title: 'Subscribe',
-        description: '',
-        url: 'YOUR_SUBSCRIBE_LINK_HERE',
-        linkTypeClass: 'subscribe-button'
-    },
-    {
         type: 'link',
         iconClass: 'fab fa-apple',
         title: 'Kho IPA',
@@ -21,7 +12,7 @@ const linkData = [
         iconClass: 'fab fa-telegram-plane',
         title: 'Grup Telegram',
         description: 'Trả Đổi Công Nghệ',
-        url: 'YOUR_TELEGRAM_GROUP_LINK_HERE',
+        url: 'https://t.me/addlist/6AxIXU4bsLRjYzJl',
         linkTypeClass: 'telegram-link'
     },
     {
@@ -29,7 +20,7 @@ const linkData = [
         iconClass: 'fab fa-apple',
         title: 'Website bán chứng chỉ',
         description: '',
-        url: 'YOUR_CERTIFICATE_SALES_WEBSITE_LINK_HERE',
+        url: 'https://chungchiontop.site',
         linkTypeClass: 'apple-link'
     },
     {
@@ -37,19 +28,7 @@ const linkData = [
         iconImage: 'https://i.imgur.com/bXed84d.png', // Ví dụ: 'https://i.imgur.com/your-ontop-logo.png'
         title: 'ON^TOP IOS',
         description: 'Server discord',
-        url: 'YOUR_ONTOP_DISCORD_SERVER_LINK_HERE',
+        url: 'https://discord.gg/M8jbZwy9',
         linkTypeClass: 'ontap-link'
     },
-    {
-        type: 'sectionTitle',
-        title: 'App IPA'
-    },
-    {
-        type: 'link',
-        iconClass: 'fab fa-youtube',
-        title: 'Youtube mod ipa',
-        description: 'Unlock pip,chặn quảng cáo',
-        url: 'YOUR_YOUTUBE_MOD_IPA_LINK_HERE',
-        linkTypeClass: 'youtube-link'
-    }
 ];
